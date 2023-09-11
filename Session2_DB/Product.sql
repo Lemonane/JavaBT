5a2b5751e535a5a0219b427c82659a20102a97ee
Lemonane <143387154+Lemonane@users.noreply.github.com>
2023-09-12 00:34:37 +0900

    upload db

---
+++ Session2_DB/Product.sql
@@ -0,0 +1,3 @@
+INSERT INTO shop.Product (Product_id, Category_name, Price, Description, Category_status) VALUES ('1', 'Bàn ghế ', null, '', true);
+INSERT INTO shop.Product (Product_id, Category_name, Price, Description, Category_status) VALUES ('2', 'Sách vở', null, '', false);
+INSERT INTO shop.Product (Product_id, Category_name, Price, Description, Category_status) VALUES ('3', 'Quần áo', null, '', true);
---
+++ Session2_DB/orders.sql
@@ -0,0 +1,2 @@
+INSERT INTO shop.orders (order_id, created, total_amount, order_status) VALUES (1, '2023-09-11', 3, false);
+INSERT INTO shop.orders (order_id, created, total_amount, order_status) VALUES (2, '2023-09-12', 1, true);
