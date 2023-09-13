1258056cc19e6da607d6a7811396fd5ccb315270
Lemonane <143387154+Lemonane@users.noreply.github.com>
2023-09-13 14:45:29 +0900

    Delete Session2_DB directory

--- Session2_DB/Product.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO shop.Product (Product_id, Category_name, Price, Description, Category_status) VALUES ('1', 'Bàn ghế ', null, '', true);
-INSERT INTO shop.Product (Product_id, Category_name, Price, Description, Category_status) VALUES ('2', 'Sách vở', null, '', false);
-INSERT INTO shop.Product (Product_id, Category_name, Price, Description, Category_status) VALUES ('3', 'Quần áo', null, '', true);
