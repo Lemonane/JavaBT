63396740a4029acb0f0d8c710c83ddac124f82e3
Lemonane <143387154+Lemonane@users.noreply.github.com>
2023-09-27 17:44:34 +0900

    fix code

--- btth_ss7/src/main/java/org/example/Dao/ReportDao.java
+++ btth_ss7/src/main/java/org/example/Dao/ReportDao.java
@@ -16,11 +16,11 @@ public class ReportDao {
         String storedProc = "{call ReportNumberOfCategoriesByStatus()}";
         try (CallableStatement cs = connection.prepareCall(storedProc)) {
             ResultSet rs = cs.executeQuery();
-            System.out.println("CatalogStatus | NumberOfCategories");
+            System.out.println("Trạng thái danh mục - số lượng");
             while (rs.next()) {
                 boolean status = rs.getBoolean("CatalogStatus");
                 int count = rs.getInt("NumberOfCategories");
-                System.out.println(status + " | " + count);
+                System.out.println(status + " : " + count);
             }
         }
     }
@@ -29,11 +29,11 @@ public class ReportDao {
         String storedProc = "{call ReportNumberOfProductsByStatus()}";
         try (CallableStatement cs = connection.prepareCall(storedProc)) {
             ResultSet rs = cs.executeQuery();
-            System.out.println("ProductStatus | NumberOfProducts");
+            System.out.println("Trạng thái sản phẩm - sô lượng");
             while (rs.next()) {
                 boolean status = rs.getBoolean("ProductStatus");
                 int count = rs.getInt("NumberOfProducts");
-                System.out.println(status + " | " + count);
+                System.out.println(status + " : " + count);
             }
         }
     }
@@ -42,11 +42,11 @@ public class ReportDao {
         String storedProc = "{call ReportNumberOfProductsByCategory()}";
         try (CallableStatement cs = connection.prepareCall(storedProc)) {
             ResultSet rs = cs.executeQuery();
-            System.out.println("CatalogName | NumberOfProducts");
+            System.out.println("Tên danh mục - có số lượng sản phẩm");
             while (rs.next()) {
                 String catalogName = rs.getString("CatalogName");
                 int count = rs.getInt("NumberOfProducts");
-                System.out.println(catalogName + " | " + count);
+                System.out.println(catalogName + " : " + count);
             }
         }
     }
